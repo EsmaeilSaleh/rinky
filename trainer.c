752f3658d7961a4e1e04fd48dd5d5869af2c23e2
EsmaeilSaleh <e.saleh88@gmail.com>
2025-05-28 12:01:29 +0200

    Auto-commit on save

--- trainer.c
+++ trainer.c
@@ -16,7 +16,7 @@ int main() {
     SetTargetFPS(60);
     srand(time(NULL));
 
-    Font customFont = LoadFont("Robot-Regular.ttf");
+    Font customFont = LoadFont("Roboto-Regular.ttf");
 
     // ✅ Proper font load check
     if (customFont.baseSize == 0 || customFont.glyphCount == 0) {
