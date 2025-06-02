3a753fb71dfd1acd006a32136999aa45664dacca
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-02 04:33:29 +0200

    update

--- trainer.c
+++ trainer.c
@@ -78,7 +78,7 @@ int main() {
 		DrawText("Type this word:", 50, 80, 24, DARKGRAY);
 
 		// Draw target word (centered, bigger)
-		DrawText(target, 150, 140, 70, DARKBLUE);
+		DrawText(target, 150, 200, 70, DARKBLUE);
 
 		// Draw input (centered, bold color)
 		DrawText(input, 50, 360, 60, MAROON);
